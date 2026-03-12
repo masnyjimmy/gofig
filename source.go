8db2715b2db5497e8145cd72798cd0ea21a403eb
masnyjimmy <kspark470@gmail.com>
2026-03-12 20:23:54 +0100

    Added yaml source

--- source.go
+++ source.go
@@ -1,8 +1,11 @@
 package gofig
 
 import (
+	"io"
 	"os"
 	"strings"
+
+	"github.com/goccy/go-yaml"
 )
 
 type Source interface {
@@ -47,3 +50,59 @@ func (this *EnvSource) Read(path string) any {
 }
 
 var _ Source = (*EnvSource)(nil)
+
+type YamlSource struct {
+	Root    string
+	records Records
+}
+
+func NewYamlSource(file string, root string) (*YamlSource, error) {
+	data, err := os.ReadFile(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	records, err := parseYAML(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &YamlSource{
+		Root:    root,
+		records: records,
+	}, nil
+}
+
+func parseYAML(data []byte) (Records, error) {
+	var raw map[string]any
+	if err := yaml.Unmarshal(data, &raw); err != nil {
+		return nil, err
+	}
+
+	records := make(Records)
+	flatten(raw, "", records)
+	return records, nil
+}
+
+func flatten(m map[string]any, prefix string, out Records) {
+	for k, v := range m {
+		key := k
+		if prefix != "" {
+			key = prefix + "." + k
+		}
+
+		switch val := v.(type) {
+		case map[string]any:
+			flatten(val, key, out)
+		default:
+			out[key] = val
+		}
+	}
+}
+
+func (this *YamlSource) Read(path string) any {
+	return this.records[path]
+}
+
+var _ Source = (*YamlSource)(nil)
